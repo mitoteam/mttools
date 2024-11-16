c0d4741a89733f3ce7aab16608f7d5146d6cb4b6
Mikhail Oleynik <mishutka@mito-team.com>
2024-11-16 16:57:37 +0300

    ExecCmdWaitAndPrint() refactored to use MultiWriter

--- exec_tools.go
+++ exec_tools.go
@@ -1,11 +1,12 @@
 package mttools
 
 import (
-	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 )
 
 // Executes cmd and returns its output or err
@@ -25,50 +26,26 @@ func ExecCmdPrint(cmd_path string, args []string) error {
 	return err
 }
 
-// Executes cmd printing its output to stdout during execution
+// Executes cmd printing its stdout+stderr to current stdout during execution and returns its output after
 func ExecCmdWaitAndPrint(cmd_path string, args []string) (output string, err error) {
 	cmd := exec.Command(cmd_path, args...)
 	//fmt.Println("CMD: " + cmd.String())
 
-	var stdout_pipe, stderr_pipe io.ReadCloser
+	var sb strings.Builder
+	mw := io.MultiWriter(&sb, os.Stdout)
 
-	if stdout_pipe, err = cmd.StdoutPipe(); err != nil {
-		return "", err
-	}
-
-	if stderr_pipe, err = cmd.StderrPipe(); err != nil {
-		return "", err
-	}
+	cmd.Stdout = mw
+	cmd.Stderr = mw
 
 	if err = cmd.Start(); err != nil {
 		return "", err
 	}
 
-	stdout_scanner := bufio.NewScanner(stdout_pipe)
-	stderr_scanner := bufio.NewScanner(stderr_pipe)
+	err = cmd.Wait()
 
-	f := true
-	var text string
-	for f {
-		stdout := stdout_scanner.Scan()
-		stderr := stderr_scanner.Scan()
+	output = sb.String()
 
-		if stdout {
-			text = stdout_scanner.Text()
-			fmt.Println(text)
-			output += text + "\n"
-		}
-
-		if stderr {
-			text = stderr_scanner.Text()
-			fmt.Println(text)
-			output += text + "\n"
-		}
-
-		f = stdout || stderr
-	}
-
-	return output, cmd.Wait()
+	return output, err
 }
 
 // Hides console only under Windows)
