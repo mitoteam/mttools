524c9d282ee078f3b6d76bffb8bf5424531d67fc
Mikhail Oleynik <mishutka@mito-team.com>
2024-11-28 18:50:25 +0300

    UniqueSlice() function added

--- slice_tools.go
+++ slice_tools.go
@@ -1,5 +1,7 @@
 package mttools
 
+import "fmt"
+
 // Returns number of `value` values found in `values_list`
 func CountValues(value interface{}, values_list ...interface{}) (count int) {
 	count = 0
@@ -10,4 +12,28 @@ func CountValues(value interface{}, values_list ...interface{}) (count int) {
 		}
 	}
 	return count
+}
+
+// Returns slice of same type with just unique elements left in it
+func UniqueSlice[S ~[]E, E any](slice S) S {
+	unique_map := make(map[string]bool, len(slice))
+
+	result := make(S, 0, len(slice))
+	for _, value := range slice {
+		var unique_key string
+		var ok bool
+
+		if unique_key, ok = any(value).(string); !ok {
+			unique_key = fmt.Sprint(value)
+		}
+
+		if len(unique_key) != 0 {
+			if !unique_map[unique_key] {
+				result = append(result, value)
+				unique_map[unique_key] = true
+			}
+		}
+	}
+
+	return result
 }
