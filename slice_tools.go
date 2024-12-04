af8b1bdc4aae9409f429f4547bca57142d0ba58a
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-04 15:54:05 +0300

    SlicesIntersection() added

--- slice_tools.go
+++ slice_tools.go
@@ -1,6 +1,9 @@
 package mttools
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 // Returns number of `value` values found in `values_list`
 func CountValues(value interface{}, values_list ...interface{}) (count int) {
@@ -32,6 +35,19 @@ func UniqueSlice[S ~[]E, E any](slice S) S {
 				result = append(result, value)
 				unique_map[unique_key] = true
 			}
+		}
+	}
+
+	return result
+}
+
+// Returns slice of values found in both slice_a and slice_b
+func SlicesIntersection[S ~[]E, E comparable](slice_a S, slice_b S) S {
+	result := make(S, 0)
+
+	for _, value := range slice_a {
+		if slices.Contains(slice_b, value) {
+			result = append(result, value)
 		}
 	}
 
