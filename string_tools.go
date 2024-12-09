6a731dd3ddd3944d7eb13d01d423eed91c1ad2e0
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-09 07:58:38 +0300

    AnyToString() function added

---
+++ string_tools.go
@@ -0,0 +1,66 @@
+package mttools
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+)
+
+// Convert any value to string. Ok = true if it is a string or Stringer, false if not.
+// https://stackoverflow.com/questions/72267243/unioning-an-interface-with-a-type-in-golang
+func AnyToStringOk(v any) (s string, b bool) {
+	switch v := v.(type) {
+	case string:
+		s = v
+
+	case fmt.Stringer:
+		s = v.String()
+
+	default:
+		// handle the remaining type set of ~string
+		r := reflect.ValueOf(v)
+		if r.Kind() == reflect.String {
+			s = r.String()
+		} else {
+			return "", false
+		}
+	}
+
+	return s, true
+}
+
+// Try best to convert any value to string, even if it is not a string at all.
+func AnyToString(v any) string {
+	if s, ok := AnyToStringOk(v); ok {
+		return s
+	}
+
+	// Last chance: lets Go render it as string.
+	return fmt.Sprintf("%v", v)
+}
+
+// Splits string into array of strings.
+// Each argument in string is single word or number, or string in double quotes.
+func SplitArgumentsString(str string) []string {
+	str = strings.TrimSpace(str)
+
+	// \p{L} = any unicode letter
+	re := regexp.MustCompile(`([\p{L}\d_\-\+\@\|\!\/]+)|(".*?[^\\]")|("")`)
+
+	result := re.FindAllString(str, -1) //-1 = all
+
+	// remove double quotes
+	for index, item := range result {
+		if len(item) > 0 && item[0] == '"' {
+			item = item[1 : len(item)-1]
+
+			result[index] = item
+		}
+	}
+
+	//DBG
+	//fmt.Printf("%+v\n", result)
+
+	return result
+}
--- text_tools.go
+++ text_tools.go
@@ -1,7 +1,6 @@
 package mttools
 
 import (
-	"regexp"
 	"strings"
 
 	lorem "github.com/drhodes/golorem"
@@ -41,31 +40,6 @@ func RandomWords(count, minLength, maxLength int, capitalize bool) string {
 			result = result + " "
 		}
 	}
-
-	return result
-}
-
-// Splits string into array of strings.
-// Each argument in string is single word or number, or string in double quotes.
-func SplitArgumentsString(str string) []string {
-	str = strings.TrimSpace(str)
-
-	// \p{L} = any unicode letter
-	re := regexp.MustCompile(`([\p{L}\d_\-\+\@\|\!\/]+)|(".*?[^\\]")|("")`)
-
-	result := re.FindAllString(str, -1) //-1 = all
-
-	// remove double quotes
-	for index, item := range result {
-		if len(item) > 0 && item[0] == '"' {
-			item = item[1 : len(item)-1]
-
-			result[index] = item
-		}
-	}
-
-	//DBG
-	//fmt.Printf("%+v\n", result)
 
 	return result
 }
