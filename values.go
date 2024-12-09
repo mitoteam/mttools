31dcb344f518db805f8fe9eeb26f44a958fb771b
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-09 22:16:02 +0300

    mttools.Values type added

---
+++ values.go
@@ -0,0 +1,37 @@
+package mttools
+
+import (
+	"iter"
+	"maps"
+)
+
+type Values struct {
+	list map[string]any
+}
+
+func NewValues() Values {
+	return Values{
+		list: make(map[string]any, 0),
+	}
+}
+
+func (m *Values) Set(key string, v any) {
+	m.list[key] = v
+}
+
+func (m *Values) GetOk(key string) (v any, ok bool) {
+	v, ok = m.list[key]
+	return v, ok
+}
+
+func (m *Values) Get(key string) any {
+	if v, ok := m.GetOk(key); ok {
+		return v
+	}
+
+	return nil
+}
+
+func (m *Values) GetNamesIterator() iter.Seq[string] {
+	return maps.Keys(m.list)
+}
