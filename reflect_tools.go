24377b33afdbc49b459d44873964a9ace1ccb7ea
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-12 17:22:56 +0300

    IsStructTypeEmbeds() added

--- reflect_tools.go
+++ reflect_tools.go
@@ -10,21 +10,24 @@ func IsStructEmbeds(s interface{}, embedded_type reflect.Type) bool {
 		return false
 	}
 
-	st := reflect.TypeOf(s)
+	return IsStructTypeEmbeds(reflect.TypeOf(s), embedded_type)
+}
 
+// Checks if s_type - is a struct (or pointer to it) and it embeds embedded_type
+func IsStructTypeEmbeds(s_type reflect.Type, embedded_type reflect.Type) bool {
 	//dereference pointer
-	if st.Kind() == reflect.Pointer {
-		st = st.Elem()
+	if s_type.Kind() == reflect.Pointer {
+		s_type = s_type.Elem()
 	}
 
 	//check if they both are structures
-	if st.Kind() != reflect.Struct || embedded_type.Kind() != reflect.Struct {
+	if s_type.Kind() != reflect.Struct || embedded_type.Kind() != reflect.Struct {
 		return false
 	}
 
 	//check fields
-	for i := 0; i < st.NumField(); i++ {
-		struct_field := st.Field(i)
+	for i := 0; i < s_type.NumField(); i++ {
+		struct_field := s_type.Field(i)
 
 		if struct_field.Type == embedded_type {
 			return true
