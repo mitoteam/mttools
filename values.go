acc5cb60591bc535a0d093f99131f98eeb6501fc
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-27 14:29:26 +0300

    Values.Has() added

--- values.go
+++ values.go
@@ -36,11 +36,17 @@ func (m *Values) GetOk(key string) (v any, ok bool) {
 // Gets value by key, returns nil if there is no such key.
 // Use GetOk() if you need distinguish nil values from key absence.
 func (m *Values) Get(key string) any {
-	if v, ok := m.GetOk(key); ok {
+	if v, ok := m.list[key]; ok {
 		return v
 	}
 
 	return nil
+}
+
+// Returns true if value for key was set.
+func (m *Values) Has(key string) bool {
+	_, ok := m.list[key]
+	return ok
 }
 
 func (m *Values) GetNamesIterator() iter.Seq[string] {
