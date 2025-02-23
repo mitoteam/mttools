a0dbcc0e2a05348c8a2e9cbae25329bba2a684f8
Mikhail Oleynik <mishutka@mito-team.com>
2025-02-23 09:13:21 +0300

    AskUserChoiceMultiple() func added

--- user_choice_tools.go
+++ user_choice_tools.go
@@ -3,36 +3,39 @@ package mttools
 import (
 	"bufio"
 	"fmt"
-	"log"
-	"maps"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func AskUserChoiceMultiple(prompt string, options_list []string) []int {
+func AskUserChoiceMultiple(prompt string, options_list []string) (choice_list []int, err error) {
 	for k, v := range options_list {
 		fmt.Printf("%2d: %s\n", k+1, v)
 	}
 
+	//Ask user
 	fmt.Print("*** " + prompt)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	err := scanner.Err()
+
+	err = scanner.Err()
 	if err != nil {
-		log.Fatal(err)
+		return []int{}, err
 	}
 
+	// Preprocess
 	user_input := strings.TrimSpace(scanner.Text())
 	user_input = strings.ReplaceAll(user_input, ",", " ")
 
 	number_string_list := strings.Split(user_input, " ")
-	numbers_list := make(map[int]int, len(number_string_list))
 
 	if len(user_input) == 0 {
-		return []int{} //empty array
+		return []int{}, nil //empty slice
 	}
+
+	// Set answers to slice
+	choice_list = make([]int, 0, len(number_string_list))
 
 	for _, v := range number_string_list {
 		if len(v) < 1 {
@@ -41,16 +44,15 @@ func AskUserChoiceMultiple(prompt string, options_list []string) []int {
 
 		n, err := strconv.Atoi(v)
 		if err != nil {
-			log.Printf("Skipping wrong input: %s (%s)", v, err.Error())
-			continue
+			return choice_list, fmt.Errorf("wrong input: %s (%s)", v, err.Error())
 		}
 
 		if n < 1 || n > len(options_list) {
 			continue
 		}
 
-		numbers_list[n] = n - 1
+		choice_list = append(choice_list, n-1)
 	}
 
-	return maps.Values(numbers_list)
+	return UniqueSlice(choice_list), nil
 }
